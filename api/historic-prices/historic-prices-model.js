0242ded44eba25f00d00b872405170f31aa6355b
nhamilton1 <nickh3592@gmail.com>
2022-01-07 12:07:58 -0500

    fixes model

--- api/historic-prices/historic-prices-model.js
+++ api/historic-prices/historic-prices-model.js
@@ -1,17 +1,20 @@
 const db = require('../data/db-config')
 
+const findAll = () => {
+    return db('historical_prices')
+}
+
 const findBetweenDates = async (startDate, endDate) => {
-    const dates = await db('btc_historical_price as btc')
-        .leftJoin('gld_historical_price as gld', 'gld.gld_date', 'btc.btc_date')
-        .leftJoin('spy_historical_price as spy', 'spy.spy_date', 'btc.btc_date')
-        .select('btc.btc_date', 'btc.btc_price', 'gld.gld_price', 'spy.spy_price')
-        .where('btc.btc_date', '>=', startDate)
-        .where('btc.btc_date', '<', endDate)
-        .orderBy('btc_date', 'asc')
+    const dates = await db('historical_prices')
+        .select('date', 'btc_price', 'gld_price', 'spy_price')
+        .where('date', '>=', startDate)
+        .where('date', '<', endDate)
+        .orderBy('date', 'ASC')
     return dates
 
 }
 
 module.exports = {
+    findAll,
     findBetweenDates
 }
