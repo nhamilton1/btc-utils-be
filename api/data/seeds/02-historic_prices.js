cf78d78514afa5a7adcc9fbb9b63ffd49b167b4c
nhamilton1 <nickh3592@gmail.com>
2022-01-08 08:54:40 -0500

    this time i think i fixed the seeded data

--- api/data/seeds/02-historic_prices.js
+++ api/data/seeds/02-historic_prices.js
@@ -2,23 +2,24 @@ const btc = require('../json/btc_historic.json')
 const spy = require('../json/spy_historic.json')
 const gld = require('../json/gld_historic.json')
 
+let newSpy = []
+let newGld = []
 // finds the missing weekend dates from the spy and gld seed and adds the btc date and a null price. 
-let newSpy = [...spy]
-let newGld = [...gld]
 btc.map(x => spy.map(s => s.spy_date).indexOf(x.btc_date)  === -1 ? newSpy.push({spy_date: x.btc_date, spy_price: null}) : "")
 btc.map(x => gld.map(s => s.gld_date).indexOf(x.btc_date)  === -1 ? newGld.push({gld_date: x.btc_date, gld_price: null}) : "")
+// adds already existing dates
+spy.map(x => newSpy.push({spy_date: x.spy_date, spy_price: x.spy_price}))
+gld.map(x => newGld.push({gld_date: x.gld_date, gld_price: x.gld_price}))
+//sorts by the date
+newSpy.sort((a,b) => new Date(a.spy_date) - new Date(b.spy_date))
+newGld.sort((a,b) => new Date(a.gld_date) - new Date(b.gld_date))
 
-let dates = btc.map(x => x.btc_date),
-btc_prices = btc.map(x => x.btc_price),
-spy_prices = newSpy.map(x => x.spy_price),
-gld_prices = newGld.map(x => x.gld_price)
-
-const data = dates.map((date, idx) => {
+const data = btc.map((date, idx) => {
   return {
-    date,
-    btc_price: btc_prices[idx],
-    spy_price: spy_prices[idx],
-    gld_price: gld_prices[idx]
+    date: btc[idx].btc_date,
+    btc_price: btc[idx].btc_price,
+    spy_price: newSpy[idx].spy_price,
+    gld_price: newGld[idx].gld_price
   }
 })
 
