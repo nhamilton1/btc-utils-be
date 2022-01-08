c22dbdc7406e905b96084996f4a89517ccb1305a
nhamilton1 <nickh3592@gmail.com>
2022-01-07 22:20:24 -0500

    I think i fixed my null bug from my seeded date

--- api/data/seeds/02-historic_prices.js
+++ api/data/seeds/02-historic_prices.js
@@ -1,25 +1,28 @@
-const btc_price = require('../json/btc_historic.json')
-const spy_price = require('../json/spy_historic.json')
-const gld_price = require('../json/gld_historic.json')
+const btc = require('../json/btc_historic.json')
+const spy = require('../json/spy_historic.json')
+const gld = require('../json/gld_historic.json')
 
+// finds the missing weekend dates from the spy and gld seed and adds the btc date and a null price. 
+btc.map(x => spy.map(s => s.spy_date).indexOf(x.btc_date)  === -1 ? spy.push({spy_date: x.btc_date, spy_price: null}) : "")
+btc.map(x => gld.map(s => s.gld_date).indexOf(x.btc_date)  === -1 ? gld.push({gld_date: x.btc_date, gld_price: null}) : "")
 
-const dates = btc_price.map(x => x.btc_date),
-btc_prices = btc_price.map(x => x.btc_price),
-spy_prices = spy_price.map(x => x.spy_price),
-gld_prices = gld_price.map(x => x.gld_price)
+let dates = btc.map(x => x.btc_date),
+btc_prices = btc.map(x => x.btc_price),
+spy_prices = spy.map(x => x.spy_price),
+gld_prices = gld.map(x => x.gld_price)
 
 const data = dates.map((date, idx) => {
-    return {
-        date,
-        btc_price: btc_prices[idx],
-        spy_price: spy_prices[idx],
-        gld_price: gld_prices[idx]
-    }
-})
 
+  return {
+    date,
+    btc_price: btc_prices[idx],
+    spy_price: spy_prices[idx],
+    gld_price: gld_prices[idx]
+  }
+})
 
 exports.data = data
 
-exports.seed = function(knex) {
-  return knex('historical_prices').insert(data)
+exports.seed = async (knex) => {
+  return await knex('historical_prices').insert(data)
 };
