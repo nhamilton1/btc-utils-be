dc8553f641344a9657874e9cddc0ea461b6b6829
nhamilton1 <nickh3592@gmail.com>
2022-01-09 08:54:29 -0500

    fixes issue: < currdate rather <= currdate

--- api/historic-prices/historic-prices-model.js
+++ api/historic-prices/historic-prices-model.js
@@ -4,7 +4,7 @@ const findBetweenDates = async (startDate, endDate) => {
   const dates = await db("historical_prices")
     .select("date", "btc_price", "gld_price", "spy_price")
     .where("date", ">=", startDate)
-    .where("date", "<", endDate)
+    .where("date", "<=", endDate)
     .orderBy("date", "ASC");
   return dates;
 };
