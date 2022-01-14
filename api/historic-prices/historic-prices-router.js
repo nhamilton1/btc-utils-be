7b10d73e5ecd485a8f1bd1265f72dce94e35779f
nhamilton1 <nickh3592@gmail.com>
2022-01-13 19:05:29 -0500

    formatting

--- api/historic-prices/historic-prices-router.js
+++ api/historic-prices/historic-prices-router.js
@@ -1,16 +1,19 @@
-const router = require('express').Router()
-const HistoricPrice = require('./historic-prices-model')
-const {scrapeDates} = require('./historic-prices-middleware')
+const router = require("express").Router();
+const HistoricPrice = require("./historic-prices-model");
+const { scrapeDates } = require("./historic-prices-middleware");
 
-router.get('/:date_string?', scrapeDates, async (req, res, next) => {
-    const startDate = req.query.startDate
-    const endDate = req.query.endDate
-    try {
-        const btc_historic = await HistoricPrice.sqlRawFindBetweenDates(startDate, endDate)
-        res.json(btc_historic)
-    } catch(err) {
-        next(err)
-    }
-})
+router.get("/:date_string?", scrapeDates, async (req, res, next) => {
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+  try {
+    const btc_historic = await HistoricPrice.sqlRawFindBetweenDates(
+      startDate,
+      endDate
+    );
+    res.json(btc_historic);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,26 +1,24 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
+const normalDistRouter = require("../api/normalDist/normalDistRouter");
+const historicPriceRouter = require("./historic-prices/historic-prices-router");
 
-const normalDistRouter = require('../api/normalDist/normalDistRouter')
-const historicPriceRouter = require('./historic-prices/historic-prices-router')
+const server = express();
 
-const server = express()
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+server.use("/api/nd", normalDistRouter);
+server.use("/api/historic_prices", historicPriceRouter);
 
-server.use('/api/nd', normalDistRouter)
-server.use('/api/historic_prices', historicPriceRouter)
-
-
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {// eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
   });
 });
 
-module.exports = server
+module.exports = server;
