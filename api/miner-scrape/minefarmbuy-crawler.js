0da9856dde93805bddae56bf07d6840375dd2007
nhamilton1 <nickh3592@gmail.com>
2022-01-30 13:16:30 -0500

    adds scrape for miners that have an efficiency option

--- api/miner-scrape/minefarmbuy-crawler.js
+++ api/miner-scrape/minefarmbuy-crawler.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 const minefarmbuyScraper = async () => {
   let browser;
   try {
-    browser = await puppeteer.launch({ headless: false, slowMo: 10 });
+    browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto("https://minefarmbuy.com/product-category/btc-asics/", {
       waitUntil: "domcontentloaded",
@@ -40,15 +40,12 @@ const minefarmbuyScraper = async () => {
           node.map((th) => th.innerText)
         );
 
-        //checks for efficiency dropdown
+        //checks for efficiency dropdown when page first loads
         const efficiency = await page.$$eval("#efficiency > option", (node) =>
           node.map((th) => th.innerText)
         );
 
-        //checks for hashrate dropdown
-        const option = await page.$$eval("#hashrate > option", (node) =>
-          node.map((th) => th.innerText)
-        );
+        const filteredEfficiency = efficiency.filter((t) => t.match(/J\/th/i));
 
         const ifNoPriceFromAsicPrice = await page.$eval(
           "div > div.summary.entry-summary > p > span > bdi",
@@ -56,11 +53,17 @@ const minefarmbuyScraper = async () => {
         );
 
         //no incoterms dropdown and no efficiency dropdown
-        if (incoterms.length === 0 && efficiency.length === 0) {
+        if (incoterms.length === 0 && filteredEfficiency.length === 0) {
+          //checks for hashrate dropdown when page first loads
+          const option = await page.$$eval("#hashrate > option", (node) =>
+            node.map((th) => th.innerText)
+          );
+
           //filters values for only ths, no batches or option value
           const filteredOptions = option.filter((t) => {
             return t.match(/th/i) && parseInt(Number(t.charAt(0)));
           });
+
 
           //loops through the filtered options and sets the data
           for (let i = 0; i < filteredOptions.length; i++) {
@@ -85,6 +88,51 @@ const minefarmbuyScraper = async () => {
             });
           }
         }
+
+        // else if (filteredEfficiency.length > 0) {
+        //   for (let i = 0; i < filteredEfficiency.length; i++) {
+        //     await page.select("select#efficiency", filteredEfficiency[i]);
+
+        //     const optionForEff = await page.$$eval(
+        //       "#hashrate > option",
+        //       (node) => node.map((th) => th.innerText)
+        //     );
+
+        //     const filteredOptionsForEff = optionForEff.filter((t) => {
+        //       return t.match(/th/i) && parseInt(Number(t.charAt(0)));
+        //     });
+
+        //     for (let j = 0; j < filteredOptionsForEff.length; j++) {
+        //       await page.select("select#hashrate", filteredOptionsForEff[j]);
+
+        //       const asicPrice = await page.$$eval(
+        //         "div > div > form > div > div > div > span > span > bdi",
+        //         (node) => node.map((price) => price.innerText)
+        //       );
+
+        //       const asicName = await page.$eval(
+        //         "div > div.summary.entry-summary > h1",
+        //         (el) => el.innerText
+        //       );
+
+        //       minefarmbuyData.push({
+        //         seller: "minefarmbuy",
+        //         asic: `${asicName} ${filteredOptionsForEff[j]} ${filteredEfficiency[i]}`,
+        //         price: asicPrice[0],
+        //       });
+        //     }
+
+        //     //waits for clear button to appear, probably can be optimized
+        //     //this is getting stuck going to just use page.reload of now
+        //     // await page.waitForSelector('.reset_variations', {visible: true})
+        //     // await page.click(".reset_variations");
+        //     // await page.waitForSelector(".woocommerce-variation single_variation", { hidden:true });
+
+        //     await page.reload({ waitUntil: "domcontentloaded" });
+        //   }
+        // }
+
+
       } catch (error) {
         console.error(error);
       }
