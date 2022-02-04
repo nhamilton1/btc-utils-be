59961bf161ef21aa5860d988811cb5c4bd9ed1fd
Nick <nickh3592@gmail.com>
2022-02-04 18:20:07 -0500

    Trying to push this

---
+++ api/data/migrations/2_miner_data.js
@@ -0,0 +1,13 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable("miner_data", (table) => {
+      table.string("model");
+      table.float("th");
+      table.float("watts");
+      table.float("efficiency");
+    });
+  };
+  
+  exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists("miner_data");
+  };
+  
