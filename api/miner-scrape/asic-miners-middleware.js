350f32e0a7bdd443f87dd94bd10f312b4536de1b
Nick <nickh3592@gmail.com>
2022-02-05 22:20:44 -0500

    fixes duplicates on the first time the data gets stored to the db

--- api/miner-scrape/asic-miners-middleware.js
+++ api/miner-scrape/asic-miners-middleware.js
@@ -1,6 +1,6 @@
 const Asics = require("./asic-miners-model");
-const MarketData = require('./market-data-model')
-const MinerData = require('./miner-data-model')
+const MarketData = require("./market-data-model");
+const MinerData = require("./miner-data-model");
 const { kaboomracksScraper } = require("./kaboomracks-crawler");
 const { minefarmbuyScraper } = require("./minefarmbuy-crawler");
 // this takes up too much memory for heroku and would have to pay to use it.
@@ -9,12 +9,12 @@ const { minefarmbuyScraper } = require("./minefarmbuy-crawler");
 const asicData = async (req, res, next) => {
   try {
     const asic = await Asics.getAllIds();
-    const minerInfo = await MinerData.getMinerData()
-    const marketInfo = await MarketData.getMarketData()
+    const minerInfo = await MinerData.getMinerData();
+    const marketInfo = await MarketData.getMarketData();
     const scrapeForMFBData = await minefarmbuyScraper();
     const scrapeForKaboomData = await kaboomracksScraper();
     const allData = scrapeForMFBData.concat(scrapeForKaboomData);
-
+    
     const minerInfoDupCheck = allData.filter(
       (scapeData) =>
         !minerInfo.find((allAsicData) => scapeData.model === allAsicData.model)
@@ -25,20 +25,25 @@ const asicData = async (req, res, next) => {
         !marketInfo.find((allAsicData) => scapeData.id === allAsicData.id)
     );
 
-    
-
     if (asic.length === 0) {
-      await MarketData.addMarketData(allData)
-      await MinerData.addMinerData(allData)
+      const minerInfoFirstDupCheck = scrapeForMFBData.filter(
+        (scapeData) =>
+          !scrapeForKaboomData.find(
+            (allAsicData) => scapeData.model === allAsicData.model
+          )
+      );
+      const firstDataInput = minerInfoFirstDupCheck.concat(scrapeForKaboomData);
+      await MinerData.addMinerData(firstDataInput);
+      await MarketData.addMarketData(allData);
       next();
     } else if (minerInfoDupCheck.length > 0) {
-      await MinerData.addMinerData(minerInfoDupCheck)
+      await MinerData.addMinerData(minerInfoDupCheck);
       next();
-    } else if (MarketData.length > 0) {
-      await MarketData.addMarketData(marketInfoDupCheck)
+    } else if (marketInfoDupCheck.length > 0) {
+      await MarketData.addMarketData(marketInfoDupCheck);
       next();
     } else {
-      next()
+      next();
     }
   } catch (err) {
     next(err);
