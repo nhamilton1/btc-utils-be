b408af22343b5a649c2b8158251c8c4144bea47f
Nick <nickh3592@gmail.com>
2022-02-06 08:56:16 -0500

    going back to less memory usage mfb scrape

--- api/miner-scrape/asic-miners-middleware.js
+++ api/miner-scrape/asic-miners-middleware.js
@@ -2,9 +2,10 @@ const Asics = require("./asic-miners-model");
 const MarketData = require("./market-data-model");
 const MinerData = require("./miner-data-model");
 const { kaboomracksScraper } = require("./kaboomracks-crawler");
-// const { minefarmbuyScraper } = require("./minefarmbuy-crawler");
+const { minefarmbuyScraper } = require("./minefarmbuy-crawler");
 // this takes up too much memory for heroku and would have to pay to use it.
-const { mfbScraper } = require("./newMFB-crawler");
+// took up mem=643M(125.7%)
+// const { mfbScraper } = require("./newMFB-crawler");
 
 const asicData = async (req, res, next) => {
   console.time('time')
@@ -12,7 +13,7 @@ const asicData = async (req, res, next) => {
     const asic = await Asics.getAllIds();
     const minerInfo = await MinerData.getMinerData();
     const marketInfo = await MarketData.getMarketData();
-    const scrapeForMFBData = await mfbScraper();
+    const scrapeForMFBData = await minefarmbuyScraper();
     const scrapeForKaboomData = await kaboomracksScraper();
     const allData = scrapeForMFBData.concat(scrapeForKaboomData);
     
