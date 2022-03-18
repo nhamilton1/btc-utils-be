7a5235303ab5a02c6fdacd609300a7d78cd5f1a0
Nicholas Hamilton <nickh3592@gmail.com>
2022-03-18 08:02:19 -0400

    removes bin

--- bin/data/db-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const knex_1 = require("knex");
-const configs = require('../../knexfile');
-const env = process.env.NODE_ENV;
-exports.db = (0, knex_1.knex)(configs[env]);
