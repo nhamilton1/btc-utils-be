be931c76096f3ed000f3acc8907785aa75e05740
Nicholas Hamilton <nickh3592@gmail.com>
2022-04-04 16:38:28 -0400

    adds upstreamdata scrape

--- api/miner-scrape/upstreamdata/upstreamdata-crawler.ts
+++ api/miner-scrape/upstreamdata/upstreamdata-crawler.ts
@@ -1,54 +1,116 @@
-import moment from "moment"
-import Puppeteer from "puppeteer"
+import moment from "moment";
+import Puppeteer from "puppeteer";
+import { sha1 } from "../helpers";
 
 interface removeImgEtcInterface {
-    resourceType: () => string;
-    abort: () => void;
-    continue: () => void;
-  }
+  resourceType: () => string;
+  abort: () => void;
+  continue: () => void;
+}
+
+interface upstreamdataInterface {
+  vendor: string;
+  model: string;
+  th: number;
+  efficiency: number;
+  watts: number;
+  price: number;
+  date: string;
+  id: string;
+}
 
 const upStreamDataCrawler = async () => {
-    let browser;
-    try {
-        browser = await Puppeteer.launch({
-            slowMo: 5,
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
-        await page.setRequestInterception(true);
+  let browser;
+  try {
+    browser = await Puppeteer.launch({
+      slowMo: 5,
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.setRequestInterception(true);
 
-        page.on("request", (req: removeImgEtcInterface): void => {
-            if (
-                req.resourceType() == "stylesheet" ||
-                req.resourceType() == "font" ||
-                req.resourceType() == "image"
-            ) {
-                req.abort();
-            } else {
-                req.continue();
-            }
-        });
+    page.on("request", (req: removeImgEtcInterface): void => {
+      if (
+        req.resourceType() == "stylesheet" ||
+        req.resourceType() == "font" ||
+        req.resourceType() == "image"
+      ) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
 
-        await page.goto("https://shop.upstreamdata.ca/product-category/asics/", {
-            waitUntil: "domcontentloaded",
-        });
+    await page.goto("https://shop.upstreamdata.ca/product-category/asics/", {
+      waitUntil: "domcontentloaded",
+    });
 
-        //gets all the urls on the btc asic product list page
-        const urls = await page.$$eval(
-            "#main > ul > li > a:first-child",
-            (title: { href: string }[]): string[] =>
-              title.map((url: { href: string }): string => url.href)
-          );
-          
+    //gets all the urls on the btc asic product list page
+    const urls = await page.$$eval(
+      "#main > ul > li > a:first-child",
+      (title: { href: string }[]): string[] =>
+        title.map((url: { href: string }): string => url.href)
+    );
 
-        console.log(urls);
-    } catch (err) {
-        console.log(err)
+    const upstreamdataAsics: upstreamdataInterface[] = [];
+
+    for (const url of urls) {
+      await page.goto(url, { waitUntil: "domcontentloaded" });
+
+      // .slice(0, -3) removes the h/s from the title
+      const asicModel: string = await page.$eval(
+        "div > div.summary.entry-summary > h1",
+        (el: { innerText: string }): string => el.innerText.slice(0, -3)
+      );
+
+      // .replace(/[^\d.-]/g, '') removes all the non-numbers from the efficiency
+      const efficiency: number = await page.$eval(
+        "#tab-description > p:nth-child(4)",
+        (el: { innerText: string }): number =>
+          Number(el.innerText.replace(/[^\d.-]/g, ""))
+      );
+
+      const watts: number = await page.$eval(
+        "#tab-description > p:nth-child(3)",
+        (el: { innerText: string }): number =>
+          Number(el.innerText.replace(/[^\d.-]/g, ""))
+      );
+
+      const price: number = await page.$eval(
+        "div > div.summary.entry-summary > p.price > span > bdi",
+        (el: { innerText: string }): number =>
+          Number(el.innerText.replace(/[^\d.-]/g, ""))
+      );
+
+      const vendor: string = "upstreamdata";
+
+      const date: string = moment(new Date()).format("MM-DD-YYYY");
+
+      const th: number = Number(asicModel.split(" ").pop()?.slice(0, -1));
+
+      const id = sha1(`upstreamdata ${asicModel} ${price}`);
+
+      upstreamdataAsics.push({
+        vendor,
+        model: asicModel,
+        th,
+        watts,
+        efficiency,
+        price,
+        date,
+        id,
+      });
     }
+    await browser.close();
+    console.log(upstreamdataAsics);
+    return upstreamdataAsics;
+  } catch (err) {
+    console.log("Could not create a browser instance => : ", err);
+  }
+};
 
-
-}
-upStreamDataCrawler()
+upStreamDataCrawler();
 
 export default upStreamDataCrawler;
