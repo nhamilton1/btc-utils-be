5fa4125730c2b7d2b56c28fc8310cc909c4106b7
Nicholas Hamilton <nickh3592@gmail.com>
2022-04-07 20:29:58 -0400

    not sure if my prisma file structure is correct

--- api/miner-scrape/models/asic-miners-model.ts
+++ api/miner-scrape/models/asic-miners-model.ts
@@ -1,56 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const main = async () => {
-
-  const allAsics = await prisma.marketData.findMany({
-    
-  })
-  console.log(allAsics)
-
-  return allAsics
-}
-
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
-
-
-// import { db } from "../../data/db-config";
-
-
-// export const getAll = async () => {
-//   const asics = await db("market_data as market")
-//     .join("miner_data as miner", "miner.model", "market.model")
-//     .select(
-//       "market.vendor",
-//       "market.price",
-//       "market.date",
-//       "market.model",
-//       "miner.th",
-//       "miner.watts",
-//       'miner.efficiency'
-//     ).orderBy('date', 'desc')
-//   return asics;
-// };
-
-// export const getAllIds = async () => {
-//   const asics = await db("market_data as market")
-//     .join("miner_data as miner", "miner.model", "market.model")
-//     .select(
-//       "market.id",
-//       "market.vendor",
-//       "market.price",
-//       "market.date",
-//       "market.model",
-//       "miner.th",
-//       "miner.watts",
-//       'miner.efficiency'
-//     );
-//   return asics;
-// };
+export const getAll = async () => {
+  const allAsics = await prisma.$queryRaw`
+    SELECT 
+    market_data.vendor, market_data.price, market_data.date, market_data.model, 
+    miner_data.th, miner_data.watts, miner_data.efficiency
+    FROM "market_data"
+    INNER JOIN "miner_data" ON market_data.model = miner_data.model  
+  `;
+  console.log(allAsics);
+  return allAsics;
+};
