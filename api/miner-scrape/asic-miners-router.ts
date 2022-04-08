eab07d0acc4a30f9661f30431c43496034d84b57
Nicholas Hamilton <nickh3592@gmail.com>
2022-04-07 20:30:27 -0400

    not sure if prisma file sctructure is correct

--- api/miner-scrape/asic-miners-router.ts
+++ api/miner-scrape/asic-miners-router.ts
@@ -1,12 +1,20 @@
 import asicData from "./asic-miners-middleware";
 import { getAll } from "./models/asic-miners-model";
 import * as express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get("/", async (_req, res, next) => {
   try {
-    const asicMiners = await getAll();
+    const asicMiners = await getAll()
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
     res.json(asicMiners);
   } catch (err) {
     next(err);
@@ -15,7 +23,13 @@ router.get("/", async (_req, res, next) => {
 
 router.get("/asics-scheduler", asicData, async (_req, res, next) => {
   try {
-    const asicMiners = await getAll();
+    const asicMiners = await getAll()
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
     res.json(asicMiners);
   } catch (err) {
     next(err);
